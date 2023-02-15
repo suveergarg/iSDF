3a28ce6cea4e762aed39efe09e3acac62141b112
gsuveer <suveer.garg@samsung.com>
2023-02-15 14:00:26 -0500

    viz

--- isdf/modules/trainer.py
+++ isdf/modules/trainer.py
@@ -2225,7 +2225,7 @@ class Trainer():
         dim = 50
         x = torch.linspace(x_min,x_max, steps=dim, device='cpu')
         y = torch.linspace(y_min,y_max, steps=dim, device='cpu')
-        z = torch.linspace(0.1, 0.5, steps=dim, device='cpu')
+        z = torch.linspace(0.0, 1, steps=dim, device='cpu')
 
         grid = torch.meshgrid(x, y, z)
         
