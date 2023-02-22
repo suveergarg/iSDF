23bf63861ec420724deed8d41ad439cda3eb0b5e
gsuveer <suveer.garg@samsung.com>
2023-02-22 12:13:00 -0500

    speed?

--- isdf/modules/trainer.py
+++ isdf/modules/trainer.py
@@ -1533,15 +1533,18 @@ class Trainer():
         batch = control_points.shape[0]
         time = control_points.shape[1]
 
-        control_points = control_points.reshape(-1, 3)
+        #control_points = control_points.reshape(-1, 3)
         
         with torch.set_grad_enabled(False):
+            sdf = self.sdf_map(control_points)
+            '''
             sdf = fc_map.chunks(
                 control_points,
                 self.chunk_size,
                 self.sdf_map,
             )
-            sdf = sdf.reshape(batch, time)
+            '''
+            #sdf = sdf.reshape(batch, time)
         
         return sdf
     
