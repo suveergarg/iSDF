27201edaa250f79ad8eee3dcc9742ff9dc067a1e
gsuveer <suveer.garg@samsung.com>
2023-02-22 12:24:33 -0500

    speed?

--- isdf/modules/trainer.py
+++ isdf/modules/trainer.py
@@ -1533,7 +1533,7 @@ class Trainer():
         batch = control_points.shape[0]
         time = control_points.shape[1]
 
-        #control_points = control_points.reshape(-1, 3)
+        control_points = control_points.reshape(-1, 3)
         
         with torch.set_grad_enabled(False):
             #sdf = self.sdf_map(control_points)
@@ -1544,7 +1544,7 @@ class Trainer():
                 self.sdf_map,
             )
             
-            #sdf = sdf.reshape(batch, time)
+            sdf = sdf.reshape(batch, time)
         
         return sdf
     
